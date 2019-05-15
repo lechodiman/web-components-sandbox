18f511246abbd88574be80785b324dfe25dd8cc6
lechodiman <lechodiman@uc.cl>
2019-05-15 18:56:23 -0400

    Extend built in element

---
+++ confirm-link.js
@@ -0,0 +1,17 @@
+// Extending specific element
+class ConfirmLink extends HTMLAnchorElement {
+  constructor() {
+    super();
+  }
+
+  connectedCallback() {
+    this.addEventListener("click", event => {
+      if (!confirm("Do you really want to leave")) {
+        event.preventDefault();
+      }
+    });
+  }
+}
+
+// When extending an specific element, we add it as third argument
+customElements.define("uc-confirm-link", ConfirmLink, { extends: "a" });
