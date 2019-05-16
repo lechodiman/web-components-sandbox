6519b1be37b24f5520579dc2efdb2dc678365e51
lechodiman <lechodiman@uc.cl>
2019-05-15 22:31:36 -0400

    Styling modal

---
+++ modal.js
@@ -0,0 +1,43 @@
+class Modal extends HTMLElement {
+  constructor() {
+    super();
+
+    this.attachShadow({ mode: "open" });
+
+    this.shadowRoot.innerHTML = `
+    <style>
+      #backdrop {
+        position: fixed;
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 100vh;
+        background: rgba(0, 0, 0, 0.75);
+        z-index: 10;
+      }
+
+      #modal {
+        position: fixed;
+        top: 15vh;
+        left: 25%;
+        width: 50%;
+        z-index: 100;
+        background: white;
+        height: 30rem;
+        border-radius: 3px;
+        box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
+      }
+    </style>  
+    
+    <div id="backdrop">
+
+    </div>
+    
+    <div id="modal">
+      A modal
+    </div>
+    `;
+  }
+}
+
+customElements.define("uc-modal", Modal);
