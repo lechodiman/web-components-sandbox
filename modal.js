74a2cb19f8974eba88ac39fd6778fe672292a30b
lechodiman <lechodiman@uc.cl>
2019-05-15 22:42:26 -0400

    General modal logic

--- modal.js
+++ modal.js
@@ -14,8 +14,10 @@ class Modal extends HTMLElement {
         height: 100vh;
         background: rgba(0, 0, 0, 0.75);
         z-index: 10;
+        opacity: 0;
+        pointer-events: none;
       }
-
+      
       #modal {
         position: fixed;
         top: 15vh;
@@ -23,9 +25,36 @@ class Modal extends HTMLElement {
         width: 50%;
         z-index: 100;
         background: white;
-        height: 30rem;
         border-radius: 3px;
         box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
+        display: flex;
+        flex-direction: column;
+        justify-content: space-between;
+        opacity: 0;
+        pointer-events: none;
+      }
+
+      header {
+        padding: 1rem;
+      }
+
+      header h1 {
+        font-size: 1.25rem;
+      }
+
+      #main {
+        padding: 1rem;
+      }
+
+      #actions {
+        border-top: 1px solid #ccc;
+        padding: 1rem;
+        display: flex;
+        justify-content: flex-end;
+      }
+
+      #actions button {
+        margin: 0 0.25rem;
       }
     </style>  
     
@@ -34,7 +63,20 @@ class Modal extends HTMLElement {
     </div>
     
     <div id="modal">
-      A modal
+      <header>
+        <h1>Please confirm</h1>
+      </header>
+
+      <section id="main">
+        <slot>
+          Some Default
+        </slot>
+      </section>
+
+      <section id="actions">
+        <button>Cancel</button>
+        <button>Okey</button>
+      </section>
     </div>
     `;
   }
