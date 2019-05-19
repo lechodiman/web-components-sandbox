e3b93967987ffd01ddf186266e3903fa35daca16
lechodiman <lechodiman@uc.cl>
2019-05-18 23:41:17 -0400

    Animate modal

--- modal.js
+++ modal.js
@@ -18,19 +18,19 @@ class Modal extends HTMLElement {
         pointer-events: none;
       }
       
-      :host([opened]) #backdrop {
-        opacity: 1;
-        pointer-events: all;
-      }
-
+      :host([opened]) #backdrop,
       :host([opened]) #modal {
         opacity: 1;
         pointer-events: all;
       }
 
+      :host([opened]) #modal {
+        top: 15vh;
+      }
+
       #modal {
         position: fixed;
-        top: 15vh;
+        top: 10vh;
         left: 25%;
         width: 50%;
         z-index: 100;
@@ -42,14 +42,17 @@ class Modal extends HTMLElement {
         justify-content: space-between;
         opacity: 0;
         pointer-events: none;
+        transition: all 0.3s ease-out;
       }
 
       header {
         padding: 1rem;
+        border-bottom: 1px solid #ccc;
       }
 
       ::slotted(h1) {
         font-size: 1.25rem;
+        margin: 0;
       }
 
       #main {
@@ -95,8 +98,12 @@ class Modal extends HTMLElement {
       console.dir(slots[1].assignedNodes());
     });
 
+    const backdrop = this.shadowRoot.querySelector("#backdrop");
+
     const cancelButton = this.shadowRoot.querySelector("#cancel-btn");
     const confirmButton = this.shadowRoot.querySelector("#confirm-btn");
+
+    backdrop.addEventListener("click", this._cancel.bind(this));
 
     cancelButton.addEventListener("click", this._cancel.bind(this));
     confirmButton.addEventListener("click", this._confirm.bind(this));
@@ -127,7 +134,7 @@ class Modal extends HTMLElement {
     this.isOpen = false;
   }
 
-  _cancel(event) {
+  _cancel() {
     this.hide();
     // Composed: if true, the event may leave the shadow DOM.
     // Bubble: if event not handled by element, it will bubble to its parent
@@ -136,7 +143,7 @@ class Modal extends HTMLElement {
     this.dispatchEvent(cancelEvent);
   }
 
-  _confirm(event) {
+  _confirm() {
     this.hide();
     const confirmEvent = new Event("confirm");
     this.dispatchEvent(confirmEvent);
